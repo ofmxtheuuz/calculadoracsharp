1c9065f7405993b7e00eee0955e2c3ab9988e912
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-12-30 03:35:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,68 +2,88 @@
 
 namespace MyApp 
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {   
-            Console.Title = "Calculadora Simples do hx";
-            Console.WriteLine("Insira o seu nome abaixo:");
-            string nome = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine($"Olá, {nome} seja muito bem-vindo(a) a calculadora!");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Qual seu primeiro número selecionado?");
-            decimal numero1 = decimal.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine($"Ótimo, {nome}! O Seu primeiro número é {numero1}");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Qual será o seu segundo número?");
-            decimal numero2 = decimal.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("Otímo! Quase lá, o que você deseja fazer?");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("1. Multiplicação");
-            Console.WriteLine("2. Adição");
-            Console.WriteLine("3. Subtração");
-            int escolha = int.Parse(Console.ReadLine());
-            if (escolha == 1)
-            {
+            try { 
+                // configs
+                Console.Title = "Calculadora Simples do hx";
+
+                // auth
+                Console.WriteLine("Insira o seu nome abaixo:");
+                string name = Console.ReadLine();
                 Console.Clear();
-                decimal resultado = numero1 * numero2;
-                Console.WriteLine($"Seu resultado é: {resultado}");
-                Console.WriteLine("");
+                Console.WriteLine($"Olá, {name} seja muito bem-vindo(a) a calculadora!");
                 Console.WriteLine("");
                 Console.WriteLine("");
 
-
-            }
-            if (escolha == 2)
-            {
+                // calculator
+                Console.WriteLine("Qual seu primeiro número selecionado?");
+                decimal number = decimal.Parse(Console.ReadLine());
                 Console.Clear();
-                decimal resultado = numero1 + numero2;
-                Console.WriteLine($"Seu resultado é: {resultado}");
+                Console.WriteLine($"Ótimo, {name}! O Seu primeiro número é {number}");
                 Console.WriteLine("");
                 Console.WriteLine("");
-                Console.WriteLine("");
-
-
-            }
-            if (escolha == 3)
-            {
+                Console.WriteLine("Qual será o seu segundo número?");
+                decimal number2 = decimal.Parse(Console.ReadLine());
                 Console.Clear();
-                decimal resultado = numero1 - numero2;
-                Console.WriteLine($"Seu resultado é: {resultado}");
+                Console.WriteLine("Otímo! Quase lá, o que você deseja fazer?");
                 Console.WriteLine("");
                 Console.WriteLine("");
-                Console.WriteLine("");
+                Console.WriteLine("1. Multiplicação");
+                Console.WriteLine("2. Adição");
+                Console.WriteLine("3. Subtração");"
+                Console.WriteLine("4. Divisão");
 
-
+                // user choose
+                int choose = int.Parse(Console.ReadLine());
+                if (choose == 1)
+                {
+                    // *
+                    Console.Clear();
+                    decimal result = number * number2;
+                    Console.WriteLine($"Seu resultado é: {result}");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                if (choose == 2)
+                {
+                    // +
+                    Console.Clear();
+                    decimal result = number + number2;
+                    Console.WriteLine($"Seu resultado é: {result}");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                if (choose == 3)
+                {
+                    // -
+                    Console.Clear();
+                    decimal result = number - number2;
+                    Console.WriteLine($"Seu resultado é: {result}");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                if (choose == 4) 
+                {
+                    Console.Clear();
+                    decimal result = number / number2;
+                    decimal r = number % number2;
+                    Console.WriteLine($"Seu resultado é: {result} ({r} de resto)");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+            } catch(e) {
+                // an error ocurred
+                Console.WriteLine("Ocorreu um erro!")
+                // exit with error code zero
+                Environment.Exit(0)
             }
-
-
         }
     }
 }
